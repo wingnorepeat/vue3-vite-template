2ca92860e650775e9b7efcd0bec68a3b81598c55
wing <730051080@qq.com>
2022-12-18 16:50:34 +0800

    修改vitepress配置文件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Vue3 Library',
   description: 'Vue3 Library',
+  base: '/vue3-vite-template/',
   themeConfig: {
     nav: [
       { text: 'Guide', link: '/guide' },
